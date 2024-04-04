1f154a5f205dada6e55f649827415e1d0dedf4ec
1Dollas <125376478+1Dollas@users.noreply.github.com>
2024-04-04 20:49:58 -0300

    Add files via upload

---
+++ main (4).php
@@ -0,0 +1,20 @@
+<?php
+$numero = 0;
+$total = 0;
+$numero_media = 0;
+$resultado = 0;
+
+echo"Escolha numeros positivos e finalize com UM NUMERO NEGATIVO\n";
+while (1){
+    $numero = readline("Digite outro numero: ");
+    
+    if ($numero < 0){
+        break;
+    }else if ($numero > 0){
+        $total = $total + $numero; 
+        $numero_media++;
+    }
+
+}   
+    $resultado = $total / $numero_media;
+    echo $resultado;
